6eefac5ccc1008cbb66ede2abddfbc71c09dd4d2
GhostHashing <ghosthashing@protonmail.com>
2020-06-05 01:36:39 -0400

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,3 +1,3 @@
-import GhostHashing
+import GhostHashing #Imports the GhostHashing.py file if it is in the current directory. If not, please move it to it.
 
 print(GhostHashing.Hash("Hello World!", "28e9uasd", 64)) #Hashes the string "Hello World!" with salt 28e9uasd with finally hashes the result 64 times with SHA256
