941367a38fb3c1995fd791d1e0eae74431662f70
GhostHashing <66456377+GhostHashing@users.noreply.github.com>
2020-06-05 00:12:47 -0400

    Create Example.py

---
+++ Example.py
@@ -0,0 +1,3 @@
+import GhostHashing
+
+print(GhostHashing.Hash("Hello World!", "28e9uasd", 64)) #Hashes the string "Hello World!" with salt 28e9uasd with finally hashes the result 64 times with SHA256
